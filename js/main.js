f1b049ce94c027bd167a5c9bcfa836d7e0f690ab
Maxim Hrichina <scorpiomax90@gmail.com>
2021-11-27 01:32:23 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+
+function request() {
+	const xhr = new XMLHttpRequest();
+
+	xhr.open(
+		'GET',
+		'https://jsonplaceholder.typicode.com/todos',
+	);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.responseType = 'json';
+
+	xhr.send();
+
+	xhr.onload = xhr.onerror = function (event) {
+		if (this.status === 200) {
+			console.log(`Ответ от сервера получен успешно, статус: ${this.status}`);
+		} else {
+			console.log(`Ошибка соединения, статус: ${this.status}`);
+		};
+	};
+
+	xhr.onreadystatechange = function (event) {
+		if (event.target.readyState === 4 &&
+			event.target.status === 200) {
+			let todos = xhr.response;
+			let data = [];
+			let users = [];
+			for (let todo of todos) {
+				data.push(todo);
+			};
+
+			users.push(data.filter(el => el.userId === 2).slice(0, 5));
+			users.push(data.filter(el => el.userId === 4).slice(0, 5));
+			users.push(data.filter(el => el.userId === 6).slice(0, 5));
+
+			function createElem(idElem) {
+				return document.createElement(idElem);
+			};
+
+			function querySelect(idElem) {
+				return document.querySelector(idElem);
+			};
+
+			let h2, inpt, ul, li;
+
+			let countOne = 0;
+			let countTwo = 0;
+			let countThree = 0;
+
+			let userOne = querySelect('#user_1');
+			let userTwo = querySelect('#user_2');
+			let userThree = querySelect('#user_3');
+
+			function createContent() {
+				let divs = document.querySelectorAll('.list');
+				for (let i = 0; i < divs.length; i++) {
+					h2 = createElem('h2');
+					divs[i].appendChild(h2);
+					h2.innerText = `To-Do list for user №${users[i][i].userId}`;
+				};
+
+				for (let item of users[0]) {
+					inpt = createElem('input');
+					inpt.disabled = true;
+					inpt.type = 'text';
+					ul = createElem('ul');
+					li = createElem('li');
+
+					userOne.appendChild(ul).appendChild(li).appendChild(inpt);
+					inpt.value = `${++countOne}. ${item.title}`;
+				};
+
+				for (let item of users[1]) {
+					inpt = createElem('input');
+					inpt.disabled = true;
+					inpt.type = 'text';
+					ul = createElem('ul');
+					li = createElem('li');
+
+					userTwo.appendChild(ul).appendChild(li).appendChild(inpt);
+					inpt.value = `${++countTwo}. ${item.title}`;
+				};
+
+				for (let item of users[2]) {
+					inpt = createElem('input');
+					inpt.disabled = true;
+					inpt.type = 'text';
+					ul = createElem('ul');
+					li = createElem('li');
+
+					userThree.appendChild(ul).appendChild(li).appendChild(inpt);
+					inpt.value = `${++countThree}. ${item.title}`;
+				};
+			};
+			createContent();
+		};
+	};
+};
+request();
